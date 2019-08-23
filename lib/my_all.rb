5646d2d9d61d4ed6fa408d451e36fb38153529c9
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-23 22:15:17 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,16 @@
-require 'pry'
+Let's just add an #include? method to determine the return value of the my_all? method. After we have the return value, we are all set. The final product will look like this:
 
 def my_all?(collection)
-  i = 0 
-  while i <
-  collection.length
-    yield(collection[i])
-    i = 1 + 1
-  end  
-
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
